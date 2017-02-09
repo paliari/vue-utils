c209930c9e88ffa70df2c48cfd9e215d57a89bbb
Marcos Paliari <marcos@paliari.com.br>
2017-02-09 16:04:23 -0200

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import Vuex from 'vuex'
+import VuexMapper from 'lib/VuexMapper'
+Vue.use(VuexMapper, Vuex)
+
+import './filters'
+import './directives'
+import './components'
