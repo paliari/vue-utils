8d36ea4c5947b14440449a63479aca3cd1425663
Marcos Paliari <marcos@paliari.com.br>
2017-02-09 16:03:37 -0200

    add components/index

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Back from './Back'
+import HeaderBar from './HeaderBar'
+import Modal from './Modal'
+import Checkbox from './forms/Checkbox'
+import IosSwitch from './forms/IosSwitch'
+import Radio from './forms/Radio'
+import SideMenu from './menu/SideMenu'
+import SideMenuItems from './menu/SideMenuItems'
+
+export default {
+  Back,
+  HeaderBar,
+  Modal,
+  Checkbox,
+  IosSwitch,
+  Radio,
+  SideMenu,
+  SideMenuItems
+}
+
+export { Back }
+export { HeaderBar }
+export { Modal }
+export { Checkbox }
+export { IosSwitch }
+export { Radio }
+export { SideMenu }
+export { SideMenuItems }
